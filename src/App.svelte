803353f2b1d1b5730b9509e86576fb321e83e00e
Gilmore Davidson <gilmoreorless@gmail.com>
2026-06-06 15:56:08 +1000

    Move tournament selection into debug controls and add UI to toggle them

--- src/App.svelte
+++ src/App.svelte
@@ -50,23 +50,16 @@
 
   let matches: Promise<MatchData[]> = $derived(loadData(currentTournament));
 
-  // Testing only!
+  // Debugging controls only
+  let showDebug = $state(false);
   const localOffset = new Date().getTimezoneOffset() / 60 * -1;
   let debugZone = $state(localOffset);
 </script>
 
-<main>
+<main class="app {showDebug ? 'debug' : ''}">
   <div class="options">
-    <label for="tournament">Tournament:</label>
-    <select id="tournament" bind:value={currentTournament}>
-      {#each Object.entries(tournaments) as tournament}
-        <option value={tournament[0]}>{tournament[1]}</option>
-      {/each}
-    </select>
-  </div>
-  <div class="debug-controls">
-    <label for="debug-time-zone"><strong>[DEBUG]</strong> Time zone UTC offset (hours):</label>
-    <input type="number" id="debug-time-zone" bind:value={debugZone} min="-14" max="14">
+    <input type="checkbox" id="debug-mode" bind:checked={showDebug}>
+    <label for="debug-mode">Show debugging controls</label>
   </div>
 
   <svelte:boundary>
@@ -77,9 +70,32 @@
       <p class="loading">Loading schedule&hellip;</p>
     {/snippet}
   </svelte:boundary>
+
+  <div class="debug-controls">
+    <h2>[DEBUG]</h2>
+
+    <div class="debug-option">
+      <label for="debug-tournament">Tournament:</label>
+      <select id="debug-tournament" bind:value={currentTournament}>
+        {#each Object.entries(tournaments) as tournament}
+          <option value={tournament[0]}>{tournament[1]}</option>
+        {/each}
+      </select>
+    </div>
+
+    <div class="debug-option">
+      <label for="debug-time-zone">Time zone UTC offset (hours):</label>
+      <input type="number" id="debug-time-zone" bind:value={debugZone} min="-14" max="14">
+    </div>
+  </div>
 </main>
 
 <style>
+  .app {
+    margin: 0;
+    padding: 2rem;
+  }
+
   .loading {
     font-size: 2rem;
     font-style: italic;
@@ -89,27 +105,60 @@
     font-size: 1.8rem;
   }
 
+  .options {
+    float: right;
+    margin-top: 2rem;
+  }
+  @media print {
+    .options {
+      display: none;
+    }
+  }
+
+  /*** Debug section: Add `debug` class to show red lines marking A4 portrait pages ***/
+
   .debug-controls {
     display: none;
   }
-  :global(body.debug) .debug-controls {
-    --debug-colour: lightcoral;
 
-    display: block;
-    position: fixed;
-    left: 22cm;
+  @media screen {
+    .app.debug {
+      outline: 1px solid red;
+      width: 21cm;
+      background-image: linear-gradient(to bottom, transparent 0 calc(100% - 1px), red 0 100%);
+      background-size: 100% 29.7cm;
+    }
+    .app.debug .debug-controls {
+      --debug-colour: lightcoral;
 
-    font-size: 1.5em;
-    background-color: color-mix(in srgb, var(--debug-colour), white 70%);
-    border: 2px dashed color-mix(in srgb, var(--debug-colour), black 10%);
-    padding: 1em;
-  }
-  .debug-controls strong {
-    display: block;
-  }
-  .debug-controls input {
-    font-size: inherit;
-    display: block;
-    width: 5em;
+      display: block;
+      position: fixed;
+      left: 22cm;
+      top: 4rem;
+
+      font-size: 1.5em;
+      background-color: color-mix(in srgb, var(--debug-colour), white 70%);
+      border: 2px dashed color-mix(in srgb, var(--debug-colour), black 10%);
+      padding: 1em;
+    }
+
+    .debug-controls h2 {
+      font-size: 1em;
+      margin: 0;
+    }
+
+    .debug-option {
+      margin-top: 0.75em;
+
+      input, select {
+        font-size: inherit;
+        display: block;
+        width: 100%;
+      }
+
+      input[type="number"] {
+        width: 5em;
+      }
+    }
   }
 </style>
